87e4ee5cf3d4fd2447e5f04ed2e23abe6ad84ae8
Yoon_Gi_Chul <kkiyshkk0@naver.com>
2020-06-18 22:35:52 +0900

    #2020-06-18 #비대칭키를 통한 암복호 구현

---
+++ src/client/HttpConnectionExample.java
@@ -0,0 +1,161 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.KeyPair;
+import java.util.Scanner;
+
+public class HttpConnectionExample { 
+	static Scanner sc = new Scanner(System.in);
+	private final String USER_AGENT = "Mozilla/5.0";
+    public static void main(String[] args) throws Exception {
+        HttpConnectionExample http = new HttpConnectionExample(); 
+        int ch = 1;
+        String urlParameters="";
+        while(true){
+        	System.out.print("메뉴선택(1: 회원가입, 2: 퍼블릭키 등록, 3: 파일 업로드, 4: 파일 다운로드): ");
+            ch = sc.nextInt();
+            sc.nextLine();
+            if(ch==0){
+                break;
+            }
+            else if(ch==1){
+            	String name, password;
+            	System.out.print("ID입력: ");
+                name = sc.nextLine();
+                System.out.print("password입력: ");
+                password = sc.nextLine();
+                urlParameters = "name="+name+"&password="+password;
+                http.sendPost("http://ec2-18-218-11-184.us-east-2.compute.amazonaws.com/user/add",urlParameters);
+            }
+            else if(ch==2){
+            	String name, password;
+            	System.out.print("ID입력: ");
+                name = sc.nextLine();
+                System.out.print("password입력: ");
+                password = sc.nextLine();
+            	File key = makeKey();
+                urlParameters = "name="+name+"&password="+password+"&key="+key;
+                System.out.println(urlParameters);
+                http.sendPost("http://ec2-18-218-11-184.us-east-2.compute.amazonaws.com/user/key",urlParameters);
+            }
+            else if(ch==3){
+            	File file = new File("./upload.txt");; 
+            	File file_key = upload();
+                urlParameters = "file="+file+"&file_key="+file_key;
+                http.sendPost("http://ec2-18-218-11-184.us-east-2.compute.amazonaws.com/file",urlParameters);
+            }
+            else if(ch==4){
+            	System.out.print("다운로드할 파일명 입력: ");
+            	String FileID = sc.nextLine();
+            	System.out.print("ID입력: ");
+            	String name = sc.nextLine();
+            	 System.out.print("password입력: ");
+                String password = sc.nextLine();
+                urlParameters = "/{"+FileID+"}?name="+name+"&password="+password;
+                http.sendGet("http://ec2-18-218-11-184.us-east-2.compute.amazonaws.com/file"+urlParameters);
+            }
+            else if(ch==5) {
+            	//String filePath = "C:\\Users\\Owner\\Desktop\\stringText.txt";
+            	//String encryptedFilePath = "C:\\Users\\Owner\\Desktop\\encryptedText.txt";
+            	//String decryptedFilePath = "C:\\Users\\Owner\\Desktop\\decryptedText.txt";
+            	String filePath = "";
+            	String encryptedFilePath = "";
+            	String decryptedFilePath = "";
+            	
+            	System.out.println("암호화 하고자 하는 파일의 위치를 입력해주세요 : ");
+            	filePath = sc.nextLine();
+            	System.out.println("암호화된 파일을 저장할 위치를 입력해주세요 : ");
+            	encryptedFilePath = sc.nextLine();
+            	System.out.println("복호화된 파일을 저장할 위치를 입력해주세요 : ");
+            	decryptedFilePath = sc.nextLine();
+            	
+                KeyPair keyPair = RSA.keyMake();
+                RSA.encryption(filePath, encryptedFilePath, keyPair.getPublic());
+                RSA.decryption(encryptedFilePath, decryptedFilePath, keyPair.getPrivate());
+            }
+        } 
+    } 
+    
+    private void sendPost(String targetUrl, String parameters) throws Exception {
+    	URL url = new URL(targetUrl);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();    
+        con.setRequestMethod("POST"); // HTTP POST 메소드 설정
+        con.setRequestProperty("User-Agent", "test"); 
+        con.setDoOutput(true); // POST 파라미터 전달을 위한 설정 // 
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream()); 
+        wr.writeBytes(parameters); 
+        wr.flush(); 
+        wr.close();
+        try {
+        	int responseCode = con.getResponseCode(); 
+        	BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream())); 
+        	String inputLine; 
+        	StringBuffer response = new StringBuffer(); 
+        	while ((inputLine = in.readLine()) != null) { 
+        		response.append(inputLine); } in.close();
+        		System.out.println("HTTP 응답 코드 : " + responseCode); 
+        		System.out.println("HTTP body : " + response.toString()
+        	);
+        } catch(IOException e) {
+        	int responseCode = con.getResponseCode();
+        	System.out.println(responseCode+" 오류");
+        }
+    }
+    
+    private void sendGet(String targetUrl) throws Exception {
+		URL url = new URL(null, targetUrl, new sun.net.www.protocol.https.Handler());
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", "test");
+		try {
+			int responseCode = con.getResponseCode();
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+			System.out.println("HTTP 응답 코드 : " + responseCode);
+			System.out.println("HTTP body : " + response.toString());
+		} catch(IOException e) {
+        	int responseCode = con.getResponseCode();
+        	System.out.println(responseCode+" 오류");
+        }
+	}
+    
+    
+	private static File makeKey() throws IOException {
+		String public_key = "asdasd";	// 퍼블릭키 생성
+		File key = new File("./key.txt");
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter(key, false);
+			writer.write(public_key);
+			writer.flush();
+		} catch(IOException e) {	
+		}
+		return key;
+    }
+	
+	private static File upload() throws IOException {
+		String upload_key = "qweqwe";	// 파일 업로드 시 키 생성
+		File key = new File("./uploadkey.txt");
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter(key, false);
+			writer.write(upload_key);
+			writer.flush();
+		} catch(IOException e) {	
+		}
+		return key;
+    }
+}
---
+++ src/client/RSA.java
@@ -0,0 +1,140 @@
+package client;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import java.util.Base64;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class RSA {
+
+	/**
+	 * RSA 키쌍을 생성합니다
+	 * @return RSA 키쌍
+	 */
+	public static KeyPair keyMake() throws NoSuchAlgorithmException {
+		// RSA 키쌍을 생성합니다.
+		KeyPair keyPair = CipherUtil.genRSAKeyPair();
+		// PublicKey publicKey = keyPair.getPublic();
+		// PrivateKey privateKey = keyPair.getPrivate();
+
+		return keyPair;
+	}
+
+	/**
+	 * 입력받은 경로에 있는 파일을 암호화하여 암호화된 파일을 지정한 위치에 저장합니다.
+	 * @param 암호화하고자 하는 파일의 경로
+	 * @param 암호화된 파일을 저장할 위치
+	 * @param Public Key
+	 * @return 암호화된 파일의 위치
+	 */
+	public static String encryption(String filePath, String savePath, PublicKey publicKey) throws Exception {
+		
+		String plainText = fileToString(filePath); // 암호화 할 문자열 입니다.
+
+		// Base64 인코딩된 암호화 문자열 입니다.
+		String encrypted = CipherUtil.encryptRSA(plainText, publicKey);
+		System.out.println("encrypted : ↓↓↓");
+		System.out.println(encrypted + "\n");
+
+		// 공개키를 Base64 인코딩한 문자일을 만듭니다.
+//		byte[] bytePublicKey = publicKey.getEncoded();
+//		String base64PublicKey = Base64.getEncoder().encodeToString(bytePublicKey);
+//		System.out.println("Base64 Public Key : " + base64PublicKey);
+		
+		stringToFile(encrypted, savePath);
+
+		return savePath;
+	}
+
+	/**
+	 * 입력받은 경로에 있는 암호화된 파일을 복호화하여 복호화된 파일을 지정한 위치에 저장합니다.
+	 * @param 복호화하고자 하는 파일의 경로
+	 * @param 복호화된 파일을 저장할 위치
+	 * @param Private Key
+	 * @return 복호화된 파일의 위치
+	 */
+	public static String decryption(String filePath, String savePath, PrivateKey privateKey)
+			throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException,
+			IllegalBlockSizeException, IOException {
+		
+		String encrypted = fileToString(filePath);
+		
+		// 복호화 합니다.
+		String decrypted = CipherUtil.decryptRSA(encrypted, privateKey);
+		System.out.println("decrypted : ↓↓↓");
+		System.out.println(decrypted + "\n");
+
+		// 개인키를 Base64 인코딩한 문자열을 만듭니다.
+//		byte[] bytePrivateKey = privateKey.getEncoded();
+//		String base64PrivateKey = Base64.getEncoder().encodeToString(bytePrivateKey);
+//		System.out.println("Base64 Private Key : " + base64PrivateKey);
+		
+		stringToFile(decrypted, savePath);
+
+		return savePath;
+	}
+
+	/**
+	 * File to String 메소드
+	 * @param 파일의 경로
+	 * @return String 반환
+	 */
+	public static String fileToString(String filePath) throws IOException {
+		//filePath = "C:\\Users\\Owner\\Desktop\\stringText.txt";
+		String content = "";
+
+        try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
+
+            // Formatting like \r\n will be lost
+            // String content = lines.collect(Collectors.joining());
+
+            // UNIX \n, WIndows \r\n
+            content = lines.collect(Collectors.joining(System.lineSeparator()));
+            System.out.println("Contents : ↓↓↓");
+            System.out.println(content + "\n");
+
+			// File to List
+            //List<String> list = lines.collect(Collectors.toList());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error...!!!");
+        }
+        return content;
+	}
+	
+	/**
+	 * String to File 메소드
+	 * @param String
+	 * @param 지정한 경로에 파일 저장
+	 * @return 파일을 저장한 경로 반환
+	 */
+	public static String stringToFile(String str, String filePath) {
+		//filePath = "C:\\Users\\Owner\\Desktop\\stringText2.txt";
+
+		try {
+            Files.write(Paths.get(filePath), str.getBytes());
+            // encoding
+            // Files.write(Paths.get(path), content.getBytes(StandardCharsets.UTF_8));
+
+            // extra options
+            // Files.write(Paths.get(path), content.getBytes(),
+			//		StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		return filePath;
+	}
+}
